82670ba3c6f48494e8e797e29741f87675f2d8c6
heetkumar <heetkumarpatel@gmail.com>
2021-10-16 18:38:25 +0530

    validation added for the Restaurant and Food API

--- server/API/Restaurant/index.js
+++ server/API/Restaurant/index.js
@@ -2,6 +2,10 @@ import { RestaurantModel } from "../../database/restaurant";
 import express from "express";
 import passport from "passport";
 
+//Validation
+import {ValidateRestaurantCity, ValidateRestaurantSearchString} from "../../validation/restaurant";
+import {ValidateRestaurantId} from "../../validation/food";
+
 const Router = express.Router();
 
 /*
@@ -13,6 +17,9 @@ Method              GET
 */
 Router.get("/", async(req,res)=>{
     try{
+        //validation
+        await ValidateRestaurantCity(req.query);
+
         const {city} = req.query;
         const restaurants = await RestaurantModel.find({city});
         return res.status(200).json({restaurants});
@@ -30,6 +37,9 @@ Method              GET
 */
 Router.get("/:_id", async(req,res)=> {
     try{
+        //validation
+        await ValidateRestaurantId(req.params);
+
         const {_id} = req.params;
         const restaurant = await RestaurantModel.findOne(_id);
 
@@ -53,6 +63,9 @@ Method          GET
 */
 Router.get("/search", async(req,res)=>{
     try{
+        // validation
+        await ValidateRestaurantSearchString(req.body);
+
         const {searchString} = req.body;
         const restaurants = await RestaurantModel.find({
             name: {$regex: searchString, $options: "i"},

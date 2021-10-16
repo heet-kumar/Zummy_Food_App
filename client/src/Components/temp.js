198b387500184d0e38562f22684e15769f30ef68
heetkumar <heetkumarpatel@gmail.com>
2021-10-16 22:10:27 +0530

    redux implemented

--- client/src/Components/temp.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Temp = () => {
-  return <div>Hello, this is a temp!!!!</div>
-};
-
-export default Temp;
--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App.js";
+import { Provider } from "react-redux";
 
-import {Provider} from "react-redux";
 import Store from "./Redux/store";
+
+// Import css files
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 ReactDOM.render(
   <React.StrictMode>
-  <Provider store={Store}>
-  <BrowserRouter>
-    <App />
-    </BrowserRouter>
-  </Provider >
+    <Provider store={Store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
